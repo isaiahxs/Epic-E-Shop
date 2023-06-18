20f05969514a20f64839860b2d53acfca09c20dc
isaiah <isaiahsinnathamby@gmail.com>
2023-06-18 12:35:17 -0400

    can retrieve names of daily items

--- app/api/item_routes.py
+++ app/api/item_routes.py
@@ -19,18 +19,39 @@ def get_daily_items():
     result = []
 
     for item in daily_items:
+        id = item['id']
         name = item['name']
         price = item['price']
+        price_icon = item['priceIcon']
         price_icon_link = item['priceIconLink']
-        image_link = item['images']['icon']
+        # image_link = item['images']['icon']
+        images = item['images']
+        rarity = item['rarity']
+        type = item['type']
+        slug = item['slug']
+        readable_type = item['readableType']
         description = item['description']
+        bundleSet = item['bundleSet']
+        bannerText = item['bannerText']
+        history = item['history']
 
         #creating a dictionary to store each item
         item_dict = {
-            'Name': name,
-            'Price': price,
-            'Image': image_link,
-            'Description': description
+            'id': id,
+            'name': name,
+            'price': price,
+            'priceIcon': price_icon,
+            'priceIconLink': price_icon_link,
+            # 'Image': image_link,
+            'images': images,
+            'rarity': rarity,
+            'type': type,
+            'slug': slug,
+            'readableType': readable_type,
+            'description': description,
+            'bundleSet': bundleSet,
+            'bannerText': bannerText,
+            'history': history
         }
 
         #adding the dictionary to the result list
--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,9 +1,12 @@
-import {useDispatch} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 import { getDailyItems } from '../../store/items'
 import { useEffect } from 'react'
 
 const HomePage = () => {
     const dispatch = useDispatch()
+    const items = useSelector(state => state.items.dailyItems)
+    // console.log("THESE ARE OUR ITEMS", items)
+    // console.log('THIS IS OUR ITEMS[0] NAME', items?.[0]?.name)
 
     useEffect(() => {
         dispatch(getDailyItems())
@@ -12,6 +15,12 @@ const HomePage = () => {
     return (
         <div>
             <h1>HomePage</h1>
+            <h2>Today's Daily Items</h2>
+            {/* check if the items array is not empty before trying to map over it */}
+            {items.length > 0 && items.map((item, idx) => (
+                //render the name of each item
+                <div key={idx}>{item.name}</div>
+            ))}
         </div>
     )
 }
