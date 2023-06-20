63c5586ade8924a16098fdf09df7f7c8000a65e2
isaiah <isaiahsinnathamby@gmail.com>
2023-06-19 20:55:35 -0400

    added redirecting logo in nav

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -44,13 +44,13 @@ function ProfileButton({ user }) {
       </button>
       <ul className={ulClassName} ref={ulRef}>
         {user ? (
-          <>
-            <li>{user.username}</li>
-            <li>{user.email}</li>
-            <li>
+          <div>
+            <div>{user.username}</div>
+            <div>{user.email}</div>
+            <div>
               <button onClick={handleLogout}>Log Out</button>
-            </li>
-          </>
+            </div>
+          </div>
         ) : (
           <>
             <OpenModalButton
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import logo from '../../assets/images/logo.png';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
+	const history = useHistory();
 
 	return (
-		<>
-			<>
-				<NavLink exact to="/">Home</NavLink>
-			</>
-			{isLoaded && (
-				<>
-					<ProfileButton user={sessionUser} />
-				</>
-			)}
-		</>
+		<div className='nav-bar'>
+
+			<div>
+				<img src={logo} className='nav-logo' onClick={() => history.push('/')}/>
+			</div>
+
+			<div className='nav-options'>
+				<div className='nav-home'>
+					<NavLink exact to="/">Home</NavLink>
+				</div>
+				<div className='nav-shop'>Shop</div>
+				<div className='nav-about-me'>About Me</div>
+				<div className='nav-view-cart'>View Cart</div>
+				{isLoaded && (
+					<div className='user-icon'>
+						<ProfileButton user={sessionUser} />
+					</div>
+				)}
+			</div>
+
+		</div>
 	);
 }
 
