25a5ca48ff7276c78caaabddc57c55cffd160077
isaiah <isaiahsinnathamby@gmail.com>
2023-06-21 11:53:04 -0400

    AboutMe looks super - later need to reconfigure for smaller dimensions

--- react-app/src/components/AboutMe/index.js
+++ react-app/src/components/AboutMe/index.js
@@ -56,49 +56,60 @@ const AboutMe = () => {
 
             <div className='about-me-body'>
 
-                <h2>Check out my socials!</h2>
+                <h2 className='body-headers'>Check out my socials!</h2>
                 <div className='socials'>
                     <div className='linked-in'>
                         <div className='about-img-container'>
-                            <img className='social-logo' src={linkedin} alt='LinkedIn Logo' />
+                            <a href='https://www.linkedin.com/in/isaiahxs/' target='_blank' rel='noopener noreferrer'>
+                                <img className='social-logo' src={linkedin} alt='LinkedIn Logo' />
+                            </a>
                         </div>
                         <div className='social-links'>
-                            <a href='https://www.linkedin.com/in/isaiahxs/'>LinkedIn</a>
+                            <a href='https://www.linkedin.com/in/isaiahxs/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
                         </div>
                     </div>
                     <div className='github'>
                         <div className='about-img-container'>
-                            <img className='social-logo' src={github} alt='GitHub Logo' />
+                            <a href='https://github.com/isaiahxs' target='_blank' rel='noopener noreferrer'>
+                                <img className='social-logo' src={github} alt='GitHub Logo' />
+                            </a>
                         </div>
                         <div className='social-links'>
-                            <a href='https://github.com/isaiahxs'>GitHub</a>
+                            <a href='https://github.com/isaiahxs' target='_blank' rel='noopener noreferrer'>GitHub</a>
                         </div>
                     </div>
                 </div>
-
-                <h2>Check out my other projects!</h2>
+                
+                <h2 className='body-headers'>Check out my other projects!</h2>
+                <h4 className='about-body-subheading'>Initial render build may take some time to load*</h4>
                 <div className='other-projects'>
                     <div className='serene-haven'>
-                        <h3>SereneHaven</h3>
+                        {/* <h3 className='project-name'>SereneHaven</h3> */}
+                        <a className='project-name' href='https://serene-haven.onrender.com/' target='_blank' rel='noopener noreferrer'>SereneHaven</a>
                         <div className='about-img-container'>
-                            <img className='project-image' src={serene}/>
+                            <a href='https://serene-haven.onrender.com/' target='_blank' rel='noopener noreferrer'>
+                                <img className='project-image' src={serene}/>
+                            </a>
                         </div>
                         <div className='project-links'>
-                            <a href='https://serene-haven.onrender.com/'>Live Site</a>
+                            <a href='https://serene-haven.onrender.com/' target='_blank' rel='noopener noreferrer'>Live Site</a>
                             <div className='separator'>|</div>
-                            <a href='https://github.com/isaiahxs/SereneHaven'>GitHub Repo</a>
+                            <a href='https://github.com/isaiahxs/SereneHaven' target='_blank' rel='noopener noreferrer'>GitHub Repo</a>
                         </div>
                     </div>
 
                     <div className='well-done'>
-                        <h3>Well-Done</h3>
+                        {/* <h3 className='project-name'>Well-Done</h3> */}
+                        <a className='project-name' href='https://well-done.onrender.com/' target='_blank' rel='noopener noreferrer'>Well-Done</a>
                         <div className='about-img-container'>
-                            <img className='project-image' src={welldone}/>
+                            <a href='https://well-done.onrender.com/' target='_blank' rel='noopener noreferrer'>
+                                <img className='project-image' src={welldone}/>
+                            </a>
                         </div>
                         <div className='project-links'>
-                            <a href='https://well-done.onrender.com/'>Live Site</a>
+                            <a href='https://well-done.onrender.com/' target='_blank' rel='noopener noreferrer'>Live Site</a>
                             <div className='separator'>|</div>
-                            <a href='https://github.com/isaiahxs/well-done-group-project'>GitHub Repo</a>
+                            <a href='https://github.com/isaiahxs/well-done-group-project' target='_blank' rel='noopener noreferrer'>GitHub Repo</a>
                         </div>
                     </div>
                 </div>
