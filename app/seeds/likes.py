ddd9b51a1aceb593d59dfff57dc6f5f48fe7682e
isaiah <isaiahsinnathamby@gmail.com>
2023-06-22 12:32:31 -0400

    fixed inconsistency in seed data

--- app/seeds/likes.py
+++ app/seeds/likes.py
@@ -4,8 +4,8 @@ from sqlalchemy.sql import text
 def seed_likes():
 
     like1 = Like(user_id=1, item_id='61bb53b3bd358a192111d97c', value=True)
-    like2 = Like(user_id=2, item_id='61bb53b3bd358a192111d97d', value=True)
-    like3 = Like(user_id=3, item_id='61bb53b3bd358a192111d97e', value=False)
+    like2 = Like(user_id=2, item_id='61bb53b3bd358a192111d97c', value=True)
+    like3 = Like(user_id=3, item_id='61bb53b3bd358a192111d97c', value=False)
     like4 = Like(user_id=1, item_id='5d86b98d4a06b017311867de', value=True)
 
     likes_list = [
