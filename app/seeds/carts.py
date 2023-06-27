5d88050e0c9d7fcdff69570fce8900e4f3ee81fc
isaiah <isaiahsinnathamby@gmail.com>
2023-06-26 21:59:12 -0400

    changed table name in seed file

--- app/seeds/carts.py
+++ app/seeds/carts.py
@@ -19,8 +19,8 @@ def seed_carts():
 
 def undo_carts():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.carts RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.cart RESTART IDENTITY CASCADE;")
     else:
-        db.session.execute(text("DELETE FROM carts"))
+        db.session.execute(text("DELETE FROM cart"))
         
     db.session.commit()
