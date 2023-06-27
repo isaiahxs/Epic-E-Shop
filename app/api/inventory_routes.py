abad636bcc9fbca18052c45410876f2b67b76c5e
isaiah <isaiahsinnathamby@gmail.com>
2023-06-27 19:10:09 -0400

    improvements to profile/inventory

--- app/api/inventory_routes.py
+++ app/api/inventory_routes.py
@@ -6,11 +6,29 @@ from flask_login import current_user, login_user, logout_user, login_required
 
 inventory_routes = Blueprint('inventory', __name__)
 
+# @inventory_routes.route('/', methods=['GET'])
+# @login_required
+# def get_inventory():
+#     """
+#     Get the current user's inventory
+#     """
+#     inventory = Inventory.query.filter_by(user_id=current_user.id).all()
+#     return {'inventory': [inventory_item.to_dict() for inventory_item in inventory]}
+
 @inventory_routes.route('/', methods=['GET'])
 @login_required
 def get_inventory():
     """
     Get the current user's inventory
     """
-    inventory = Inventory.query.filter_by(user_id=current_user.id).all()
-    return {'inventory': [inventory_item.to_dict() for inventory_item in inventory]}
+    from sqlalchemy import func
+    inventory = db.session.query(
+        Inventory.item_id, 
+        func.sum(Inventory.quantity).label('total')
+    ).filter_by(
+        user_id=current_user.id
+    ).group_by(
+        Inventory.item_id
+    ).all()
+    
+    return {'inventory': [{'itemId': i.item_id, 'quantity': i.total} for i in inventory]}
--- react-app/src/components/InventoryPage/index.js
+++ react-app/src/components/InventoryPage/index.js
@@ -1,20 +1,90 @@
 import {useSelector, useDispatch} from 'react-redux'
-import { getDailyItems, getFeaturedItems } from '../../store/items'
+import { setSeedItems, setDailyItems, setFeaturedItems, setCurrentItem, getSeedItems, getDailyItems, getFeaturedItems } from '../../store/items'
+import { setLikes, getLikes } from '../../store/like'
+import { getComments } from '../../store/comments'
 import { useEffect } from 'react'
 import { getItemBackgroundColor } from '../../utils'
 import { useHistory } from 'react-router-dom'
+import { getInventory } from '../../store/inventory'
+import { getReminders } from '../../store/reminders'
+import './InventoryPage.css'
 
 const InventoryPage = () => {
     const dispatch = useDispatch();
     const inventory = useSelector(state => state.inventory);
+    console.log('INVENTORY', inventory)
     const sessionUser = useSelector(state => state.session.user);
     const reminders = useSelector(state => state.reminders);
+    const seedItems = useSelector(state => state.items.seedItems);
+    const dailyItems = useSelector(state => state.items.dailyItems);
+    const featuredItems = useSelector(state => state.items.featuredItems);
+
+    useEffect(() => {
+        const seedItemsStored = localStorage.getItem('seedItems');
+        const dailyItemsStored = localStorage.getItem('dailyItems');
+        const featuredItemsStored = localStorage.getItem('featuredItems');
+    
+        if (!seedItemsStored || !dailyItemsStored || !featuredItemsStored) {
+            dispatch(getSeedItems())
+            dispatch(getDailyItems())
+            dispatch(getFeaturedItems())
+        } else {
+            dispatch(setSeedItems(JSON.parse(seedItemsStored)));
+            dispatch(setDailyItems(JSON.parse(dailyItemsStored)));
+            dispatch(setFeaturedItems(JSON.parse(featuredItemsStored)));
+        }
+
+        //i want to always fetch likes from the server
+        dispatch(getLikes());
+        dispatch(getComments());
+        dispatch(getInventory());
+        dispatch(getReminders());
+    }, [dispatch]);
+
+    const allItems = [...seedItems, ...dailyItems, ...featuredItems]
 
     return (
         <>
-            <div>Your Inventory</div>
-            <div>Total items: ({inventory.length})</div>
-            <div>Your Reminders: ({reminders.length})</div>
+            {sessionUser &&
+                <>
+                    <div className='inventory-heading'>
+                        {inventory.length > 0 &&
+                            <h1 className='inventory-welcome'>Nice collection so far, {sessionUser.username}!</h1>
+                        }
+                        <h2>Member since: {sessionUser.created_at}</h2>
+                        <h2>Your Inventory value:</h2>
+                        <h2>Total items:</h2>
+                        <h2>Unique items: ({inventory.length})</h2>
+                    </div>
+
+                    {inventory.map(inventoryItem => {
+                        const item = allItems.find(item => item.itemId === inventoryItem.itemId);
+
+                        return item ? (
+                            <div className='inventory-item' key={inventoryItem.itemId}>
+                                <div className='inventory-quantity'>{inventoryItem.quantity}x</div>
+                                <div className='img-container'>
+                                    <img className='home-item-image' src={item.images.icon} alt={item.name} style={{ backgroundColor: getItemBackgroundColor(item.rarity) }}/>
+                                </div>
+                                <div className='inventory-item-info'>
+                                    <div className='inventory-item-name'>Item name: {item.name}</div>
+                                    <div className='rarity' style={{ backgroundColor: getItemBackgroundColor(item.rarity) }}>{item.rarity}</div>
+                                    <div className='inventory-item-type'>Type: {item.type}</div>
+                                    <div className='item-detail-price'>
+                                        <img src={item.priceIconLink} alt={item.priceIcon} className='vbucks-icon'/>
+                                        <div className='inventory-item-price'>{item.price}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        ) : null;
+                    })}
+
+                    <div>Your Reminders: ({reminders.length})</div>
+                </>
+            }
+            {!sessionUser &&
+                <h1>Sign in to view your account details!</h1>
+            }
         </>
     )
 }
